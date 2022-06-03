8f55df668338cd3bb6a81b37bc938bbeb4947b95
Edward Kimuhu <kihiroedward@gmail.com>
2022-06-03 15:14:47 +0300

    feat(alpha-HR) : Login functional

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {SharedService} from '../../shared/services/shared.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +9,38 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+  loginForm : FormGroup;
+
+
+  constructor(
+    private _sharedService : SharedService,
+    private _fb : FormBuilder,
+    private _router : Router
+  ) {}
 
   ngOnInit() {
+    this.loginForm = this._fb.group({
+      username: ['', Validators.required],
+      password : ['', Validators.required]
+    })
+  }
+
+  login(){
+  console.log('login Button Click');
+
+    this._sharedService.login(this.loginForm.value).subscribe(res => {
+      console.log('Login Successful', res);
+      localStorage.setItem('loginStatus', '1');
+      localStorage.setItem('jwt', res['details']['jwt']);
+      localStorage.setItem('refresh_token', res['details']['refresh_token']);
+      localStorage.setItem('access_token', res['details']['access_token']);
+      localStorage.setItem('expiry_time', res['details']['expires_in']);
+
+
+      this._router.navigate(['/profile']);
+
+
+    })
   }
   ngOnDestroy() {
   }
