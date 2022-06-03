5fbac76fc88b590346b4ddaa035b3143457684fe
Edward Kimuhu <kihiroedward@gmail.com>
2022-06-03 13:26:20 +0300

    feat(prod) : Login and Forgot password components before logic

--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -1,9 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { ForgotComponent } from "src/app/pages/forgot/forgot.component";
 
-import { LoginComponent } from '../../pages/login/login.component';
-import { RegisterComponent } from '../../pages/register/register.component';
+import { LoginComponent } from "../../pages/login/login.component";
+import { RegisterComponent } from "../../pages/register/register.component";
 
 export const AuthLayoutRoutes: Routes = [
-    { path: 'login',          component: LoginComponent },
-    { path: 'register',       component: RegisterComponent }
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "forgot", component: ForgotComponent },
 ];
